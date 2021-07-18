3dc3ed923aec983767dcbb9e05b92d7cf518ea88
Savchuk Artur <savchukartur1@gmail.com>
2021-07-18 14:34:49 +0300

    update video-popup

--- app/js/main.js
+++ app/js/main.js
@@ -1,22 +1,25 @@
-// меню
-const burger = document.querySelector('.header__burger');
-const menu = document.querySelector('.menu');
+'use strict'
 const html = document.querySelector('html');
+const menu = document.querySelector('.header__menu');
+const burger = document.querySelector('.header__burger');
+
+
+
+
 
 // пространство между элементами меню для псевдоэлемента ::after
 function calcEmptySpace() {
-    const wrapper = document.querySelector('.header__menu');
-    const items = wrapper.querySelectorAll('li');
+    const items = menu.querySelectorAll('li');
     let itemsTakeSpace = 0;
-    
+
     items.forEach(li => {
         itemsTakeSpace += li.offsetWidth;
     })
-    const emptySpace = wrapper.offsetWidth - itemsTakeSpace;
-    const emptyPart = (emptySpace / items.length) / 2;
+    const emptySpaceAll = menu.offsetWidth - itemsTakeSpace;
+    const halfEmptySpace = (emptySpaceAll / items.length) / 2;
 
     items.forEach(li => {
-        li.style.setProperty('--empty', `-${emptyPart}px`);
+        li.style.setProperty('--empty', `-${halfEmptySpace}px`);
     })
 }
 
@@ -24,12 +27,14 @@ function calcEmptySpace() {
 window.addEventListener('load', calcEmptySpace);
 window.addEventListener('resize', calcEmptySpace);
 
+// burger button
 burger.addEventListener('click', () => {
     burger.classList.toggle('open');
     menu.classList.toggle('open');
     html.classList.toggle('scroll-off');
 });
 
+// disable opened mobile menu on desktop
 const desktopBreakpoint = 1000;
 window.addEventListener('resize', () => {
     if (window.innerWidth > desktopBreakpoint) {
@@ -38,6 +43,10 @@ window.addEventListener('resize', () => {
         html.classList.remove('scroll-off');
     }
 });
+
+
+
+
 
 // fixed header
 const header = document.querySelector('.header');
@@ -52,39 +61,28 @@ function fixHeaderHeight() {
 // при загрузке пересчитываем высоту header
 window.addEventListener('load', fixHeaderHeight);
 
-// при скролле
-let prevScrollPosition = window.pageYOffset;
-const headerHeight = headerRow_1.offsetHeight
-window.onscroll = function() {
-  const currentScrollPosition = window.pageYOffset;
-  if (prevScrollPosition > currentScrollPosition) {
-    header.style.top = "0";
-  } else {
-    header.style.top = `-${headerHeight}px`;
-  }
-  prevScrollPosition = currentScrollPosition;
-}
-
 // при ресайзе пересчитываем высоту header
 window.addEventListener('resize', fixHeaderHeight);
 
-// is Apple
-function isApple() {
-    return [
-        'iPad Simulator',
-        'iPhone Simulator',
-        'iPod Simulator',
-        'iPad',
-        'iPhone',
-        'iPod'
-    ].includes(navigator.platform)
-        // iPad on iOS 13 detection
-        || (navigator.userAgent.includes('Mac') && 'ontouchend' in document);
-}
-
-window.addEventListener('load', () => {
-    if (isApple()) document.querySelector('html').classList.add('ios');
+/**
+ * when you scrolling page down - first part of header go to up
+ * when you scrolling page up - first part of header come back down
+ */
+let prevScrollPosition = window.pageYOffset;
+const heightOfFirstPart = headerRow_1.offsetHeight
+window.addEventListener('scroll', () => {
+    const currentScrollPosition = window.pageYOffset;
+    if (prevScrollPosition > currentScrollPosition) {
+        header.style.top = "0";
+    } else {
+        header.style.top = `-${heightOfFirstPart}px`;
+    }
+    prevScrollPosition = currentScrollPosition;
 });
+
+
+
+
 
 // banner
 if (document.querySelector('.banner')) {
@@ -105,7 +103,12 @@ if (document.querySelector('.banner')) {
         },
     });
 }
-// steps
+
+
+
+
+
+// section steps
 const stepsCards = document.querySelectorAll('.steps__card');
 if (stepsCards.length) {
     stepsCards.forEach(card => {
@@ -127,9 +130,9 @@ if (popups.length) {
             e.stopPropagation();
 
             let flag = false;
-            if ( e.target.classList.contains('popup__wrapper') ) {
+            if (e.target.classList.contains('popup__wrapper')) {
                 flag = true;
-            } else if ( e.target.classList.contains('popup__close') ) {
+            } else if (e.target.classList.contains('popup__close')) {
                 flag = true;
             }
 
@@ -151,7 +154,11 @@ if (popups.length) {
     });
 }
 
-// видео с ютуба
+
+
+
+
+// youtube videos
 function initVideos() {
     for (let e = $('iframe'), t = 0; t < e.length; t++) {
         if (-1 === e[t].src.indexOf("enablejsapi")) {
@@ -163,7 +170,7 @@ function initVideos() {
 
 function enableVideo(selector) {
     const video = document.querySelector(selector);
-    if(video) {
+    if (video) {
         video.addEventListener('click', e => {
             const player = e.target;
             if (player.classList.contains('pause')) {
@@ -192,44 +199,55 @@ if (popupCallers.length) {
     });
 }
 
-// price tabs
-window.addEventListener('load', () => {
+
+
+
+
+/**
+ * tabs         - common class for tabs (string)          = '.className'
+ * contentItems - common class for content items (string) = '.className'
+ */
+function enableTabs(tabs, contentItems) {
+    // on page load - first tab and it's item must be active
     document.querySelector('button[data-tab="tab-1"]').classList.add('active');
     document.querySelector('#tab-1').classList.add('active');
-})
-document.querySelectorAll('.price__tab').forEach(tab => {
-    tab.addEventListener('click', () => {
-        document.querySelectorAll('.price-card').forEach(content => {
-            content.classList.remove('active');
-        });
-        document.querySelectorAll('.price__tab').forEach(tab => {
-            tab.classList.remove('active');
-        });
 
-        let current = tab.getAttribute('data-tab');
+    document.querySelectorAll(tabs).forEach(tab => {
+        tab.addEventListener('click', () => {
+            let current = tab.getAttribute('data-tab');
 
-        tab.classList.add('active');
-        document.querySelector(`#${current}`).classList.add('active');
+            //on click remove active class from siblings
+            document.querySelectorAll(contentItems).forEach(item => {
+                item.classList.remove('active');
+            });
+            document.querySelectorAll(tabs).forEach(tab => {
+                tab.classList.remove('active');
+            });
+
+            // add active class to current tab and it's content item
+            tab.classList.add('active');
+            document.querySelector(`#${current}`).classList.add('active');
+        })
     })
-})
-// reviews tabs
-document.querySelectorAll('.reviews__tab').forEach(tab => {
-    tab.addEventListener('click', () => {
-        document.querySelectorAll('.reviews__content').forEach(content => {
-            content.classList.remove('active');
-        });
-        document.querySelectorAll('.reviews__tab').forEach(tab => {
-            tab.classList.remove('active');
-        });
+}
 
-        let current = tab.getAttribute('data-tab');
+// tabs in section price
+const priceTabs = document.querySelector('.price__card-wrapper');
+if (priceTabs) {
+    enableTabs('.price__tab', '.price-card');
+}
 
-        tab.classList.add('active');
-        document.querySelector(`#${current}`).classList.add('active');
-    })
-})
+// tabs in section reviews
+const reviewsTabs = document.querySelector('.reviews__inner');
+if (reviewsTabs) {
+    enableTabs('.reviews__tab', '.reviews__content');
+}
 
-// btn "read more" in seo
+
+
+
+
+// btn "read more" in section seo
 if (document.querySelector(".seo")) {
     const less = 'Свернуть';
     const more = 'Подробнее';
@@ -248,6 +266,10 @@ if (document.querySelector(".seo")) {
     }
 }
 
+
+
+
+// section models
 const modelsSlider = document.querySelector('.models__slider');
 if (modelsSlider) {
     new Swiper('.models__slider', {
@@ -277,6 +299,9 @@ if (modelsSlider) {
         },
     });
 }
+
+
+
 
 
 function getPlaylist(playlistID, quantityVideos = 10) {
@@ -311,58 +336,25 @@ function getPlaylist(playlistID, quantityVideos = 10) {
 
 // add video-cards
 document.querySelectorAll('.add-video-card').forEach(container => {
-    // const playlistID = parseURL( container.getAttribute('data-playlistURL') );
     const playlistID = container.getAttribute('data-playlistID');
     const quantity = +container.getAttribute('data-quantity');
     let data = getPlaylist(playlistID, quantity);
     let card;
-      
-    setTimeout(()=> {
+
+    setTimeout(() => {
         data.forEach(item => {
-            if ( container.classList.contains('reviews-youtube__slider-inner') ) {
-                card = `<div class="swiper-slide">
-                            <div class="video-card call-popup" data-popupName="video-popup" data-videoID="${item.id}">
-                                <picture class="video-card__image">
+            card = `<div class="swiper-slide">
+                        <div class="video-card" data-videoID="${item.id}">
+                            <div class="video-card__image">
+                                <a href="https://www.youtube.com/embed/${item.id}" data-fancybox>
                                     <img src="${item.thumbnails}" alt="${item.title}">
-                                </picture>
-                                <div class="video-card__title title-3">${item.title}</div>
+                                </a>
                             </div>
-                        </div>`;
-            } else {
-                card = `<div class="video-card call-popup" data-popupName="video-popup" data-videoID="${item.id}">
-                            <picture class="video-card__image">
-                                <img src="${item.thumbnails}" alt="${item.title}">
-                            </picture>
                             <div class="video-card__title title-3">${item.title}</div>
-                        </div>`;
-            }
+                        </div>
+                    </div>`;
             container.insertAdjacentHTML('beforeend', card);
         });
-
-        const popupCallers = document.querySelectorAll('.add-video-card .call-popup');
-        if (popupCallers.length) {
-            popupCallers.forEach(caller => {
-                const popupID = caller.getAttribute('data-popupName');
-                const popup = document.querySelector(`#${popupID}`);
-                caller.addEventListener('click', () => {
-                    popup.classList.add('open');
-                    html.classList.add('scroll-off');
-
-                    if (popup.getAttribute('id') === 'video-popup') {
-                        const videoID = caller.getAttribute('data-videoID');
-                        const src = `https://www.youtube.com/embed/${videoID}`;
-                        const imgURL = `https://i.ytimg.com/vi/${videoID}/hqdefault.jpg`;
-                        const title = caller.querySelector('.video-card__title').textContent;
-
-
-                        popup.querySelector('.player').setAttribute('data-src', src);
-                        popup.querySelector('.player').style.backgroundImage = `url(${imgURL})`;
-                        popup.querySelector('.popup__title').textContent = title;
-                        enableVideo('#video-popup .player');
-                    }
-                });
-            });
-        }
 
         // reviews slider
         const reviewsSlider = document.querySelector('.reviews-youtube__slider');
@@ -394,22 +386,21 @@ document.querySelectorAll('.add-video-card').forEach(container => {
     }, 500);
 })
 
-// get video id
-function youtubeParser(url){
-    var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
-    var match = url.match(regExp);
-    return (match&&match[7].length==11)? match[7] : false;
-}
+// // get video id
+// function youtubeParser(url){
+//     var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
+//     var match = url.match(regExp);
+//     return (match&&match[7].length==11)? match[7] : false;
+// }
 
-// simple-block video
-const simpleBlock = document.querySelector('.simple-block__player-wrapper');
-if (simpleBlock) {
-    const player = simpleBlock.querySelector('.player');
-    const videoID = youtubeParser( player.getAttribute('data-url'));
-    const imgURL = `https://i.ytimg.com/vi/${videoID}/hqdefault.jpg`;
-    const src = `https://www.youtube.com/embed/${videoID}`;
+// // simple-block video
+// const simpleBlock = document.querySelector('.simple-block__player-wrapper');
+// if (simpleBlock) {
+//     const player = simpleBlock.querySelector('.player');
+//     const videoID = youtubeParser( player.getAttribute('data-url'));
+//     const imgURL = `https://i.ytimg.com/vi/${videoID}/hqdefault.jpg`;
+//     const src = `https://www.youtube.com/embed/${videoID}`;
 
-    player.setAttribute('data-src', src);
-    player.style.backgroundImage = `url(${imgURL})`;
-}
-1
+//     player.setAttribute('data-src', src);
+//     player.style.backgroundImage = `url(${imgURL})`;
+// }
